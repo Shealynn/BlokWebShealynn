6fcd39e477da404b2a6a7f837470767b8c2fe3a9
Shealynn <90252869+Shealynn@users.noreply.github.com>
2021-11-11 18:12:58 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var kleurmodeButton = document.querySelector('#kleurmodebutton')
+
+function veranderKleur() {
+    var element = document.body;
+    element.classList.toggle("kleur-mode");
+}
+
+kleurmodeButton.addEventListener('click', veranderKleur);
